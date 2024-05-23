f0e816d69129f84d1bfa5b919d1c6db1f06af605
Sunny Mak <smak@google.com>
2024-05-23 19:21:47 +0000

    Add sign up related views.

---
+++ src/components/CommonDisclaimer.vue
@@ -0,0 +1,8 @@
+<template>
+  <p>
+    By continuing, you acknowledge that you understand and agree to the
+    <!-- TODO: Update links -->
+    <RouterLink to="/">Terms & Conditions</RouterLink> and
+    <RouterLink to="/">Privacy Policy</RouterLink>.
+  </p>
+</template>
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import SignUp from './components/SignUp.vue';
+import SignUpCheckEmail from './components/SignUpCheckEmail.vue';
+
 export const router = createRouter({
   history: createWebHistory('/auth/'),
-  routes: [],
+  routes: [
+    // Sign Up
+    { path: '/sign-up', component: SignUp },
+    { path: '/sign-up-check-email', component: SignUpCheckEmail },
+  ],
 });
