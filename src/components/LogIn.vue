0272b2d1e1aa59957eedc444f3ea7e90dccc381b
Sunny Mak <smak@google.com>
2024-05-24 18:34:17 +0000

    Add login and logout views.

---
+++ src/components/LogIn.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import CommonDisclaimer from './CommonDisclaimer.vue';
+import {
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+import { onBeforeUnmount, ref } from 'vue';
+
+import { REDIRECT_URI_PROP } from '../common';
+import { router } from '../router';
+
+const auth = getAuth();
+const props = defineProps(REDIRECT_URI_PROP);
+
+// Fields
+const email = ref();
+const password = ref();
+const errorMessage = ref();
+
+async function signIn() {
+  try {
+    await signInWithEmailAndPassword(auth, email.value, password.value);
+  } catch (error) {
+    // TODO: Clean up error message.
+    errorMessage.value = (error as Error).message;
+    return;
+  }
+  // Clear the error if this sign in attempt does not throw.
+  errorMessage.value = null;
+}
+
+// Listening for auth state changes.
+const unsubscribeAuthListener = onAuthStateChanged(auth, (user) => {
+  if (user) {
+    if (props.redirectUri) {
+      window.location.href = 'http://' + props.redirectUri;
+    } else {
+      router.push({ path: '/account-management' });
+    }
+  }
+});
+
+onBeforeUnmount(unsubscribeAuthListener);
+</script>
+
+<template>
+  <h2>Login</h2>
+  <form @submit.prevent :class="{ 'error-state': errorMessage }">
+    <div class="error-message">{{ errorMessage }}</div>
+    <div class="email">
+      <label for="email">Email</label>
+      <input type="email" v-model="email" />
+    </div>
+
+    <div class="password">
+      <label for="password">Password</label>
+      <input type="password" v-model="password" />
+    </div>
+
+    <button @click="signIn" type="submit">Continue</button>
+
+    <CommonDisclaimer />
+  </form>
+  <div class="no-account-sign-up">
+    <p>Don't have an account?</p>
+    <RouterLink to="/sign-up">Create account</RouterLink>
+  </div>
+</template>
--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,38 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory, RouteLocation } from 'vue-router';
 
+import LogIn from './components/LogIn.vue';
+import LogOut from './components/LogOut.vue';
 import SignUp from './components/SignUp.vue';
 import SignUpCheckEmail from './components/SignUpCheckEmail.vue';
 
 export const router = createRouter({
   history: createWebHistory('/auth/'),
   routes: [
+    {
+      path: '/',
+      component: LogIn,
+      props: (route: RouteLocation) => ({
+        redirectUri: route.query.redirect_uri,
+      }),
+    },
     // Sign Up
     { path: '/sign-up', component: SignUp },
     { path: '/sign-up-check-email', component: SignUpCheckEmail },
+    // Login
+    {
+      path: '/login',
+      component: LogIn,
+      props: (route: RouteLocation) => ({
+        redirectUri: route.query.redirect_uri,
+      }),
+    },
+    // Logout
+    {
+      path: '/logout',
+      component: LogOut,
+      props: (route: RouteLocation) => ({
+        redirectUri: route.query.redirect_uri,
+      }),
+    },
   ],
 });
