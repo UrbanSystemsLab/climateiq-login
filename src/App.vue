84637dee55cfda79df15c70d1b0dc6eb0f7b64bc
Sunny Mak <smak@google.com>
2024-06-05 16:50:54 +0000

    Exchange ID token for Apigee token when signed in.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
 <script setup lang="ts">
-import firebaseConfigStr from '../firebase-config.json?raw';
 import { initializeApp } from 'firebase/app';
+import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
+import { onBeforeUnmount } from 'vue';
+
+import { deleteCookies, getApigeeTokenAndSetCookies } from './common';
+import firebaseConfigStr from '../firebase-config.json?raw';
 
 const firebaseConfig = JSON.parse(firebaseConfigStr);
 initializeApp(firebaseConfig);
+
+const auth = getAuth();
+
+// Listening for auth state changes.
+const unsubscribeAuthListener = onAuthStateChanged(
+  auth,
+  async (user: User | null) => {
+    if (user) {
+      // User is signed in.
+      const accessToken = await user.getIdToken();
+      await getApigeeTokenAndSetCookies(accessToken);
+    } else {
+      // User is signed out.
+      deleteCookies();
+    }
+  },
+);
+
+onBeforeUnmount(unsubscribeAuthListener);
 </script>
 
 <template>
--- src/common.ts
+++ src/common.ts
@@ -1,5 +1,12 @@
+import axios from 'axios';
+import cookie from 'cookie';
 import { User, updateProfile } from 'firebase/auth';
 import { doc, Firestore, setDoc } from 'firebase/firestore';
+import moment from 'moment';
+
+const TOKEN_COOKIE_ID = 'climate_iq_access_token';
+const EXPIRY_COOKIE_ID = 'climate_iq_access_token_expiry';
+const TOKEN_API_ENDPOINT_URI = 'https://34.49.55.140.nip.io/oauth/token';
 
 export const REDIRECT_URI_PROP = {
   redirectUri: {
@@ -7,6 +14,52 @@ export const REDIRECT_URI_PROP = {
     default: '',
   },
 };
+
+export function deleteCookies() {
+  document.cookie = cookie.serialize(TOKEN_COOKIE_ID, '', {
+    maxAge: -1,
+    path: '/',
+  });
+  document.cookie = cookie.serialize(EXPIRY_COOKIE_ID, '', {
+    maxAge: -1,
+    path: '/',
+  });
+}
+
+export async function getApigeeTokenAndSetCookies(
+  firebaseToken: string | null,
+) {
+  const cookies = cookie.parse(document.cookie);
+  if (cookies[TOKEN_COOKIE_ID]) {
+    if (parseInt(cookies[EXPIRY_COOKIE_ID]) > moment().unix()) {
+      return;
+    }
+  }
+  const response = await axios.post(
+    TOKEN_API_ENDPOINT_URI,
+    {
+      grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+      assertion: firebaseToken,
+    },
+    {
+      headers: {
+        Authorization:
+          'Basic cWNRZXNsbnY5eVFTS09kbkVEWDBCTkRVM2xjTzIwOWU6ZmFkVWZ5Q21OMDg0emJJcQ==',
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    },
+  );
+  document.cookie = cookie.serialize(
+    TOKEN_COOKIE_ID,
+    response.data['access_token'],
+    { maxAge: response.data['expires_in'], path: '/' },
+  );
+  document.cookie = cookie.serialize(
+    EXPIRY_COOKIE_ID,
+    String(moment().unix() + parseInt(response.data['expires_in'])),
+    { maxAge: response.data['expires_in'], path: '/' },
+  );
+}
 
 export async function updateUserName(
   currentUser: User,
