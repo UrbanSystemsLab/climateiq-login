43814e76a4efff9e1c1bba9a07bfdf56d9312f4c
sylmak <163595985+sylmak@users.noreply.github.com>
2024-06-06 18:40:48 -0400

    Exchange ID token for Apigee token on login. (#8)
    * Exchange ID token for Apigee token when signed in.
    
    * Address comments.

--- src/common.ts
+++ src/common.ts
@@ -1,5 +1,17 @@
+import axios from 'axios';
+import cookie from 'cookie';
 import { User, updateProfile } from 'firebase/auth';
 import { doc, Firestore, setDoc } from 'firebase/firestore';
+import moment from 'moment';
+
+const TOKEN_COOKIE_ID = 'climateiq_access_token';
+const EXPIRY_COOKIE_ID = 'climateiq_access_token_expiry';
+const TOKEN_API_ENDPOINT_URI = 'https://34.49.55.140.nip.io/oauth/token';
+// This authorization code is used for identifying this user authentication app. The
+// Apigee /oauth/token endpoint will only return an API access token if a valid JWT
+// issued by the authentication app is also present in the request.
+const AUTH_CODE =
+  'cDF1QXM4T3NGdDI2emlUYWIzQmt2cFdNOUJIbDVyUmNjT25CZHhzVFIya3MwVEZ0OmNTNU1WSFFjTkI4VHJ0UXJLOVZpc09BNDFNUGVRQUtjWHc5ZmRPcTZIWFN4aDBUdU1Va0h5Tk9wZ0ZsYTBpYnI=';
 
 export const REDIRECT_URI_PROP = {
   redirectUri: {
@@ -7,6 +19,52 @@ export const REDIRECT_URI_PROP = {
     default: '',
   },
 };
+
+export function deleteCookies() {
+  document.cookie = cookie.serialize(TOKEN_COOKIE_ID, '', {
+    maxAge: -1,
+    path: '/',
+  });
+  document.cookie = cookie.serialize(EXPIRY_COOKIE_ID, '', {
+    maxAge: -1,
+    path: '/',
+  });
+}
+
+export async function getApigeeTokenAndSetCookies(
+  firebaseToken: string | null,
+) {
+  const cookies = cookie.parse(document.cookie);
+  if (cookies[TOKEN_COOKIE_ID]) {
+    if (parseInt(cookies[EXPIRY_COOKIE_ID]) > moment().unix()) {
+      return;
+    }
+  }
+
+  const response = await axios.post(
+    TOKEN_API_ENDPOINT_URI,
+    {
+      grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+      assertion: firebaseToken,
+    },
+    {
+      headers: {
+        Authorization: `Basic ${AUTH_CODE}`,
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    },
+  );
+  document.cookie = cookie.serialize(
+    TOKEN_COOKIE_ID,
+    response.data['access_token'],
+    { maxAge: response.data['expires_in'], path: '/' },
+  );
+  document.cookie = cookie.serialize(
+    EXPIRY_COOKIE_ID,
+    String(moment().unix() + parseInt(response.data['expires_in'])),
+    { maxAge: response.data['expires_in'], path: '/' },
+  );
+}
 
 export async function updateUserName(
   currentUser: User,
