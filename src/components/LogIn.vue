c29bc0e83edb4f7675377e17b0d0f57355c3e649
sylmak <163595985+sylmak@users.noreply.github.com>
2024-07-15 14:54:21 -0400

    Handle Apigee Saml Idp logins. (#20)

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -1,22 +1,42 @@
 <script setup lang="ts">
-import CommonDisclaimer from './CommonDisclaimer.vue';
 import {
   getAuth,
   onAuthStateChanged,
   signInWithEmailAndPassword,
 } from 'firebase/auth';
-import { onBeforeUnmount, ref } from 'vue';
+import { onBeforeUnmount, nextTick, ref } from 'vue';
 
-import { cookiesSet, REDIRECT_URI_PROP } from '../common';
+import {
+  cookiesSet,
+  getSamlSignedResponse,
+  REDIRECT_URI_PROP,
+} from '../common';
+import CommonDisclaimer from './CommonDisclaimer.vue';
 import { router } from '../router';
 
 const auth = getAuth();
-const props = defineProps(REDIRECT_URI_PROP);
+const props = defineProps({
+  ...REDIRECT_URI_PROP,
+  ...{
+    apigeeSamlIdpSessionId: {
+      type: String,
+      default: '',
+    },
+  },
+});
 
 // Fields
 const email = ref();
 const password = ref();
 const errorMessage = ref();
+
+// Apigee Saml Idp login fields
+const acsUrl = ref();
+const samlForm = ref<HTMLFormElement>();
+const samlResponse = ref();
+
+// Toggles
+const loggedIn = ref(false);
 
 async function signIn() {
   try {
@@ -33,8 +53,20 @@ async function signIn() {
 // Listening for auth state changes.
 const unsubscribeAuthListener = onAuthStateChanged(auth, async (user) => {
   if (user) {
+    loggedIn.value = true;
     await cookiesSet;
-    if (props.redirectUri) {
+    // If the SAML session ID was passed in the query params, get SAML token.
+    if (props.apigeeSamlIdpSessionId) {
+      const firebaseToken = await user.getIdToken();
+      const response = await getSamlSignedResponse(
+        firebaseToken,
+        props.apigeeSamlIdpSessionId,
+      );
+      acsUrl.value = response['acs_url'];
+      samlResponse.value = response['saml_response'];
+      // Wait until form element is updated.
+      nextTick(() => samlForm.value!.submit());
+    } else if (props.redirectUri) {
       window.location.href = props.redirectUri;
     } else {
       router.push({ path: '/account-management' });
@@ -46,29 +78,41 @@ onBeforeUnmount(unsubscribeAuthListener);
 </script>
 
 <template>
-  <h2>Login</h2>
-  <form @submit.prevent :class="{ 'error-state': errorMessage }">
-    <div class="error-message" v-if="errorMessage">{{ errorMessage }}</div>
-    <div class="email">
-      <label for="email">Email</label>
-      <input type="email" v-model="email" />
+  <div class="logged-out" v-show="!loggedIn">
+    <h2>Login</h2>
+    <form @submit.prevent :class="{ 'error-state': errorMessage }">
+      <div class="error-message" v-if="errorMessage">{{ errorMessage }}</div>
+      <div class="email">
+        <label for="email">Email</label>
+        <input type="email" v-model="email" />
+      </div>
+
+      <div class="password">
+        <label for="password">Password</label>
+        <input type="password" v-model="password" />
+      </div>
+
+      <div class="reset-password">
+        <RouterLink to="reset-password">Reset Password</RouterLink>
+      </div>
+
+      <button @click="signIn" type="submit">Continue</button>
+
+      <CommonDisclaimer />
+    </form>
+    <div class="no-account-sign-up">
+      <p>Don't have an account?</p>
+      <RouterLink to="/sign-up">Create account</RouterLink>
     </div>
-
-    <div class="password">
-      <label for="password">Password</label>
-      <input type="password" v-model="password" />
-    </div>
-
-    <div class="reset-password">
-      <RouterLink to="reset-password">Reset Password</RouterLink>
-    </div>
-
-    <button @click="signIn" type="submit">Continue</button>
-
-    <CommonDisclaimer />
-  </form>
-  <div class="no-account-sign-up">
-    <p>Don't have an account?</p>
-    <RouterLink to="/sign-up">Create account</RouterLink>
+  </div>
+  <div class="logged-in" v-show="loggedIn">
+    <h2>Logging in...</h2>
+  </div>
+  <!-- Because this action results in a 302 redirect, must run request in the foreground
+   by submitting through a form. -->
+  <div class="saml-idp" v-show="false">
+    <form method="post" :action="acsUrl" ref="samlForm">
+      <input type="hidden" name="SAMLResponse" v-model="samlResponse" />
+    </form>
   </div>
 </template>
